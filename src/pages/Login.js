6c9c0e106eb8cf18a3a5ecc7aee270ab58f44a27
FilipMilovanovic <coficofi4@gmail.com>
2022-09-16 01:53:01 +0200

    Login funkcija

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,38 @@
+import { useState } from 'react'
+import axios from 'axios';
 
 
 function Login() {
 
+    const [login, setLogin] = useState({
+        broj_indeksa: '',
+        password: ''
+    })
+
+    function saveBrojIndeksa(e) {
+        setLogin({ ...login, broj_indeksa: e.target.value })
+    }
+
+    function savePassword(e) {
+        setLogin({ ...login, password: e.target.value })
+    }
+
+
+    function loginFja() {
+        axios.post("http://localhost:8000/api/login", login).then(res => {
+
+            console.log(login.broj_indeksa + " " + login.password)
+
+            if (res.data.rez == 200) {
+                alert("Uspesno ste se prijavili na sistem!")
+            }
+            else
+                alert("Molimo pokusajte ponovo!")
+
+        }).catch(error => {
+            alert(error.response.data.error)
+        })
+    }
 
 
 
@@ -10,15 +41,15 @@ function Login() {
 
             <div className="brind-div">
                 <label id="lbl-broj-indeksa" className="display-6">Broj indeksa:</label>
-                <input type="text" className="form-control" id="inp-br-ind" />
+                <input type="text" className="form-control" id="inp-br-ind" onChange={saveBrojIndeksa} value={login.broj_indeksa} />
             </div>
 
             <div className="brind-div">
                 <label id="lbl-broj-indeksa" className="display-6">Lozinka:</label>
-                <input type="text" className="form-control" id="inp-br-ind" />
+                <input type="text" className="form-control" id="inp-br-ind" onChange={savePassword} value={login.password} />
             </div>
 
-            <button className="btn btn-secondary" id="btn-prijavi-se">Prijavi se</button>
+            <button onClick={loginFja} className="btn btn-secondary" id="btn-prijavi-se">Prijavi se</button>
 
         </div>
     )
